993e96f8dc3e414ea37116825b1a257e09fbe004
Sao Thien Hat <saothienhat@gmail.com>
2020-02-21 16:41:57 +0700

    Init project

---
+++ SpringBatchDemo/src/main/java/com/saothienhat/SpringBatchDemoApplication.java
@@ -0,0 +1,14 @@
+package com.saothienhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Batch demo");
+		SpringApplication.run(SpringBatchDemoApplication.class, args);
+	}
+
+}
